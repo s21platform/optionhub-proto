9898f3a249398fccaf379fcf30c5796cda04871c
Tony <akatkov89@gmail.com>
2025-04-01 23:06:34 +0300

    added message for kafka

--- generate.sh
+++ generate.sh
@@ -3,5 +3,9 @@ protoc  --go_out=./  \
         --go-grpc_out=./ \
         optionhub.v1.proto
 
+protoc  --go_out=./  \
+        --go-grpc_out=./ \
+        new_attribute.proto
+
 protoc --doc_out=. --doc_opt=markdown,README.md ./optionhub.proto
-protoc --doc_out=. --doc_opt=markdown,README_v1.md ./optionhub.v1.proto
+protoc --doc_out=. --doc_opt=markdown,README_v1.md ./optionhub.v1.proto ./new_attribute.proto
